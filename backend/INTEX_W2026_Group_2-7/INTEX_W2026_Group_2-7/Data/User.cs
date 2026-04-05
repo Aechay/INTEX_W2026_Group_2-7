2bb9a8e524a0088912eba274568731c4c9a9a5f5
Aechay <102746342+Aechay@users.noreply.github.com>
2026-04-05 15:44:43 -0600

    Add some fake migrations to see if they'll go to supabase automatically.

---
+++ backend/INTEX_W2026_Group_2-7/INTEX_W2026_Group_2-7/Data/User.cs
@@ -0,0 +1,10 @@
+namespace INTEX_W2026_Group_2_7.Data;
+
+public class User
+{
+    public int UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
