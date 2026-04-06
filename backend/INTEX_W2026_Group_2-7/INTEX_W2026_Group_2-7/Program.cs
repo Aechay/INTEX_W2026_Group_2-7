839a5b717fb6a6f62222438da76e826249b236e0
Aechay <102746342+Aechay@users.noreply.github.com>
2026-04-05 18:46:04 -0600

    Database Stuff, including a migration!

--- backend/INTEX_W2026_Group_2-7/INTEX_W2026_Group_2-7/Program.cs
+++ backend/INTEX_W2026_Group_2-7/INTEX_W2026_Group_2-7/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
